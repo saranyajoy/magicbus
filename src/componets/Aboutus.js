5cafe87143520ecc45f0909b4fcc0b7d36e66278
shiva kumar gattu <121631767+shivakumargattu@users.noreply.github.com>
2023-11-23 20:10:04 +0530

    magic bus upload

---
+++ src/componets/Aboutus.js
@@ -0,0 +1,54 @@
+import { CloudArrowUpIcon, LockClosedIcon, ServerIcon } from '@heroicons/react/20/solid'
+
+const features = [
+  {
+    name: 'Push to deploy.',
+    description:
+      'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.',
+    icon: CloudArrowUpIcon,
+  },
+  {
+    name: 'SSL certificates.',
+    description: 'Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui lorem cupidatat commodo.',
+    icon: LockClosedIcon,
+  },
+  {
+    name: 'Database backups.',
+    description: 'Ac tincidunt sapien vehicula erat auctor pellentesque rhoncus. Et magna sit morbi lobortis.',
+    icon: ServerIcon,
+  },
+]
+
+export default function Aboutus() {
+  return (
+    <div id="Aboutus" className="overflow-hidden  bg-black py-20 sm:py-32">
+      <div className="mx-auto max-w-7xl px-6 lg:px-8">
+        <div className="mx-auto grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 sm:gap-y-20 lg:mx-0 lg:max-w-none lg:grid-cols-2">
+          <div className="lg:pr-8 lg:pt-4">
+            <div className="lg:max-w-lg">
+                           <p className="mt-2 text-3xl font-bold tracking-tight text-gray-200 sm:text-4xl">About Us</p>
+              <p className="mt-6 text-lg leading-8 text-gray-600">
+              Magic Bus is one of the top five NGOs in the education and skilling space in India. Founded in Mumbai in 1999, we have grown into an organisation with a pan-India presence. Magic Bus works with young people living in poverty, taking them from a childhood full of challenges to a life with meaningful livelihoods. We work with young people's ecosystems (parents, peers, community, and local institutions) to create a network of support for them.
+              </p>
+             <br/> <br/> <p className='text-gray-400 mt-8'> <b>Mobile</b> : +91 9642656480</p><br/>
+              <p className='text-gray-400'><b>email</b> : info@magicbusindia.org</p>
+              
+            </div>
+          </div>
+          <img
+            src="https://www.magicbus.org/demo/images/about-magic-bus-01a.jpg"
+            alt="Product screenshot"
+            className="w-[48rem] max-w-none rounded-xl shadow-xl ring-1 ring-gray-400/10 sm:w-[57rem] md:-ml-4 lg:-ml-0"
+            width={2432}
+            height={1442}
+          />
+        </div>
+      </div>
+      
+    /* vision code */
+
+    
+    
+    </div>
+  )
+}
